25d28cfcad1dc60e146ecd65bc9b3153ea74c60e
Taulant74 <taulantr2004@gmail.com>
2024-02-03 22:24:33 +0100

    i fshiva edhe do file t panevojshme

--- deleteuser.php
+++ deleteuser.php
@@ -2,19 +2,40 @@
 require('connect.php');
 session_start();
 
+class UserDeleter
+{
+    private $conn;
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function deleteUser($id)
+    {
+        $delete = $this->conn->prepare("DELETE FROM `users` WHERE id = ?");
+        $delete->bind_param("i", $id);
+
+        if ($delete->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+
+        $delete->close();
+    }
+}
+
+$userDeleter = new UserDeleter($conn);
+
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    $delete = $conn->prepare("DELETE FROM `users` WHERE id = ?");
-    $delete->bind_param("i", $id);
-
-    if ($delete->execute()) {
+    if ($userDeleter->deleteUser($id)) {
         echo "User deleted successfully!";
     } else {
-        echo "Error deleting user: " . $delete->error;
+        echo "Error deleting user.";
     }
-
-    $delete->close();
 }
 
 header("Location: dashboard.php");
